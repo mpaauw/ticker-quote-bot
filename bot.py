534aa572d909f2ccbe981095ab98ca3c4c2748f1
adjutant-xx <matthew.paauw@email.wsu.edu>
2017-06-30 10:08:29 -0700

    Working code to produce a test post.

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 # COPYRIGHT (C) MATT PAAUW 2016
 # https://github.com/mpaauw/ticker-quote-bot
 
+import praw
 import configparser
 
 config = configparser.ConfigParser()
@@ -11,3 +12,7 @@ clientSecret = config.get("creds", "clientSecret")
 userAgent = config.get("creds", "userAgent")
 username = config.get("creds", "username")
 password = config.get("creds", "password")
+
+reddit = praw.Reddit(client_id=clientId, client_secret=clientSecret, password=password, user_agent=userAgent,  username=username)
+
+reddit.subreddit('test').submit('Test Post Please Ignore, Fam', url='https://reddit.com')
