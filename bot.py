4f8dd584fec6fc30ff130f6d4bb99feb67f30631
Matt <matt.paauw@gmail.com>
2017-08-22 22:40:15 -0700

    Fixed bug where local cache was not being read properly.

--- bot.py
+++ bot.py
@@ -2,6 +2,7 @@
 # https://github.com/mpaauw/ticker-quote-bot
 
 import praw
+from praw.models import MoreComments
 import configparser
 import quandl
 import datetime
@@ -21,16 +22,41 @@ call = config.get("creds", "call")
 quandl.ApiConfig.api_key = quandlKey
 reddit = praw.Reddit(client_id=clientId, client_secret=clientSecret, password=password, user_agent=userAgent,  username=username)
 
-for submission in reddit.subreddit(sub).new(limit=5):
-    for comment in submission.comments.list():
-        if not hasattr(comment, 'body'):
-            continue
-        if call in comment.body:
-            splitBody = comment.body.split('@')
-            length = len(splitBody)
-            if len(splitBody) > 1:
-                ticker = splitBody[1]
-                date = datetime.datetime.now().strftime("%Y-%m-%d")
-                data = quandl.Dataset('WIKI/' + ticker).data(params={'start_date':date, 'end_date':date, 'rows':1})
-                comment.reply('[%s] last closing price: [%s]' % (ticker, data[0].close))
-        
+with open('cache.txt', 'r+') as cache:
+
+    cacheContent = cache.readlines() # load cache content
+    cacheContent = [item.strip() for item in cacheContent] # sanitize cache content
+
+    # NOTE: remove submission limit after testing
+    # parse all new submissions within default subreddit:
+    for submission in reddit.subreddit(sub).new(limit=5): 
+        for comment in submission.comments.list():
+
+            # ignore additional comments for now:
+            if isinstance(comment, MoreComments):
+                continue
+
+            # check cache to see if comment has already been addressed:
+            if comment.fullname in cacheContent:
+                print('Comment already visited, skipping: [%s]' % (comment.fullname))
+                continue
+
+            # check to see if comment contains a body:
+            if not hasattr(comment, 'body'):
+                continue
+            
+            # parse comment for summon call:
+            if call in comment.body:
+                splitBody = comment.body.split('@')
+                if len(splitBody) > 1: 
+                    ticker = splitBody[1]
+                    date = datetime.datetime.now().strftime("%Y-%m-%d")
+                    try:
+                        data = quandl.Dataset('WIKI/' + ticker).data(params={'start_date':date, 'end_date':date, 'rows':1})
+                        comment.reply('[%s] last closing price: [%s]' % (ticker, data[0].close))
+                        print('Reply added to Comment: [%s] requesting quote for [%s]' % (comment.fullname, ticker))
+                        cache.write(comment.fullname + '\n')
+                        print('Comment added to cache: [%s]' % (comment.fullname))                       
+                    except:
+                        print('Error fetching quote: [%s]\n' % (ticker))
+                           
