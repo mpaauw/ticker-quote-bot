992a70721ea22bc18c02f5c842232e856614ef07
Matt <matt.paauw@gmail.com>
2017-08-22 22:49:25 -0700

    Added extensive comment documentation.

--- bot.py
+++ bot.py
@@ -7,9 +7,9 @@ import configparser
 import quandl
 import datetime
 
+# load config data:
 config = configparser.ConfigParser()
 config.readfp(open("credentials.config"))
-
 clientId = config.get("creds", "clientId")
 clientSecret = config.get("creds", "clientSecret")
 userAgent = config.get("creds", "userAgent")
@@ -19,17 +19,14 @@ quandlKey = config.get("creds", "quandlKey")
 sub = config.get("creds", "sub")
 call = config.get("creds", "call")
 
-quandl.ApiConfig.api_key = quandlKey
-reddit = praw.Reddit(client_id=clientId, client_secret=clientSecret, password=password, user_agent=userAgent,  username=username)
+quandl.ApiConfig.api_key = quandlKey # load quandl instance
+reddit = praw.Reddit(client_id=clientId, client_secret=clientSecret, password=password, user_agent=userAgent,  username=username) # load reddit instance via praw
 
-with open('cache.txt', 'r+') as cache:
-
+with open('cache.txt', 'r+') as cache: # open cache while processing new posts
     cacheContent = cache.readlines() # load cache content
     cacheContent = [item.strip() for item in cacheContent] # sanitize cache content
-
-    # NOTE: remove submission limit after testing
-    # parse all new submissions within default subreddit:
-    for submission in reddit.subreddit(sub).new(limit=5): 
+   
+    for submission in reddit.subreddit(sub).new(): # parse all new submissions within default subreddit (no limit):
         for comment in submission.comments.list():
 
             # ignore additional comments for now:
@@ -38,7 +35,7 @@ with open('cache.txt', 'r+') as cache:
 
             # check cache to see if comment has already been addressed:
             if comment.fullname in cacheContent:
-                print('Comment already visited, skipping: [%s]' % (comment.fullname))
+                print('Comment already visited, skipping: [%s]' % (comment.fullname)) ##
                 continue
 
             # check to see if comment contains a body:
@@ -51,12 +48,11 @@ with open('cache.txt', 'r+') as cache:
                 if len(splitBody) > 1: 
                     ticker = splitBody[1]
                     date = datetime.datetime.now().strftime("%Y-%m-%d")
-                    try:
+                    try: # attempt to retrieve and reply quote data specified within summon:
                         data = quandl.Dataset('WIKI/' + ticker).data(params={'start_date':date, 'end_date':date, 'rows':1})
-                        comment.reply('[%s] last closing price: [%s]' % (ticker, data[0].close))
-                        print('Reply added to Comment: [%s] requesting quote for [%s]' % (comment.fullname, ticker))
-                        cache.write(comment.fullname + '\n')
-                        print('Comment added to cache: [%s]' % (comment.fullname))                       
-                    except:
-                        print('Error fetching quote: [%s]\n' % (ticker))
-                           
+                        comment.reply('[%s] last closing price: [%s]' % (ticker, data[0].close)) # post reply to summoner 
+                        print('Reply added to Comment: [%s] requesting quote for [%s]' % (comment.fullname, ticker)) ##
+                        cache.write(comment.fullname + '\n') # now that summoner's comment has been addressed, cache it to avoid in the future
+                        print('Comment added to cache: [%s]' % (comment.fullname)) ##              
+                    except: # data was not fetched and / or read properly
+                        print('Error fetching quote: [%s]' % (ticker)) ##
