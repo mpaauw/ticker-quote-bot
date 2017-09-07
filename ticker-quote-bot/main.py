94080f6acad488d5c6b1bc7029a222b7ce01c946
Matt <matt.paauw@gmail.com>
2017-09-06 22:18:51 -0700

    Added metrics support to main program execution entry point.:

--- ticker-quote-bot/main.py
+++ ticker-quote-bot/main.py
@@ -1,9 +1,16 @@
 from config import Configuration
 from reddit import Reddit
 from data import Data
+from metrics import Metrics
+
+metrics = Metrics()
+metrics.start()
 
 configuration = Configuration()
-r = Reddit(configuration.clientId, configuration.clientSecret, configuration.password, configuration.userAgent, configuration.username, configuration.call)
+r = Reddit(configuration.clientId, configuration.clientSecret, configuration.password, configuration.userAgent, configuration.username, configuration.call, metrics)
 data = Data(configuration.apiKey)
 
 r.parseSubmissions(configuration.sub, data)
+
+metrics.end()
+metrics.buildReport()
