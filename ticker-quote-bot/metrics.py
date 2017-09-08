d4a6b2a61e18fc9671ff0a1a26875ab01ec83ef8
Matt <matt.paauw@gmail.com>
2017-09-08 12:53:36 -0700

    Added support for new inbox-parsing method.

--- ticker-quote-bot/metrics.py
+++ ticker-quote-bot/metrics.py
@@ -5,15 +5,34 @@ class Metrics:
     startTime = None
     endTime = None
     totalTime = None
+    totalReadItems = 0
+    totalRepliedItems = 0
+    avgReadTime = 0
+    avgReplyTime = 0
     totalComments = 0
     totalSubmissions = 0
-    avgCommentTime = None
-    avgSubmissionTime = None
-    
-    def buildReport(self):
+    avgCommentTime = 0
+    avgSubmissionTime = 0
+
+    def buildInboxReport(self):
         print('\n')
         print('================================')
         print('--- TICKER-QUOTE-BOT METRICS ---')
+        print('--------- Inbox Report ---------')
+        print('================================')
+        print('Total Items Read:            [%s]' % (self.totalReadItems))
+        print('Total Items Replied:         [%s]' % (self.totalRepliedItems))
+        print('Avg Time / Read:             [%s seconds (rough)]' % (self.avgReadTime))
+        print('Avg Time / Reply:            [%s seconds (rough)]' % (self.avgReplyTime))
+        print('Total Running Time:          [%s seconds]' % (self.totalTime))
+        print('================================')
+        print('\n')
+
+    def buildSubmissionsReport(self):
+        print('\n')
+        print('================================')
+        print('--- TICKER-QUOTE-BOT METRICS ---')
+        print('------ Submission  Report ------')
         print('================================')
         print('Total Comments Parsed:       [%s]' % (self.totalComments))
         print('Total Submissions Parsed:    [%s]' % (self.totalSubmissions))
@@ -29,15 +48,24 @@ class Metrics:
     def end(self):
         self.endTime = self.getTime()
         self.totalTime = self.endTime - self.startTime
-
+        if self.totalReadItems > 0:
+            self.avgReadTime = self.totalTime / self.totalReadItems
+        if self.totalRepliedItems > 0:
+            self.avgReplyTime = self.totalTime / self.totalRepliedItems
         if self.totalComments > 0:
             self.avgCommentTime = self.totalTime / self.totalComments
-
         if self.totalSubmissions > 0: 
             self.avgSubmissionTime = self.totalTime / self.totalSubmissions
 
     def getTime(self):
         return time.clock()
+
+    def trackItem(self, replied):
+        if(replied):
+            self.totalRepliedItems += 1
+            self.totalReadItems += 1
+        else:
+            self.totalReadItems += 1
 
     def trackComment(self):
         self.totalComments += 1
