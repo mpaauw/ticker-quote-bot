9ac2b8aad9262afba06b48b197abc56630d94df1
Matt <matt.paauw@gmail.com>
2017-09-14 18:16:46 -0700

    Updated Data class to handle new API endpoint changes.

--- ticker-quote-bot/data.py
+++ ticker-quote-bot/data.py
@@ -15,15 +15,15 @@ class Data:
         self.logger.write('Data instantiated.')
 
     def getQuote(self, ticker):
-        endpoint = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=%s&interval=1min&outputsize=compact&apikey=%s' % (ticker, self.key)
+        endpoint = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=%s&apikey=%s' % (ticker, self.key)
         response = requests.get(endpoint)
         print('Status Code: [%s]' % (response.status_code))
         try:
-            open = Decimal(str(list(json.loads(json.dumps(response.json()['Time Series (1min)'])).values())[0]['1. open'])).quantize(Decimal('.01'))
-            high = Decimal(str(list(json.loads(json.dumps(response.json()['Time Series (1min)'])).values())[0]['2. high'])).quantize(Decimal('.01'))
-            low = Decimal(str(list(json.loads(json.dumps(response.json()['Time Series (1min)'])).values())[0]['3. low'])).quantize(Decimal('.01'))
-            close = Decimal(str(list(json.loads(json.dumps(response.json()['Time Series (1min)'])).values())[0]['4. close'])).quantize(Decimal('.01'))
-            volume = Decimal(str(list(json.loads(json.dumps(response.json()['Time Series (1min)'])).values())[0]['5. volume'])).quantize(Decimal('.01'))
+            open = Decimal(str(list(json.loads(json.dumps(response.json()['Time Series (Daily)'])).values())[0]['1. open'])).quantize(Decimal('.01'))
+            high = Decimal(str(list(json.loads(json.dumps(response.json()['Time Series (Daily)'])).values())[0]['2. high'])).quantize(Decimal('.01'))
+            low = Decimal(str(list(json.loads(json.dumps(response.json()['Time Series (Daily)'])).values())[0]['3. low'])).quantize(Decimal('.01'))
+            close = Decimal(str(list(json.loads(json.dumps(response.json()['Time Series (Daily)'])).values())[0]['4. close'])).quantize(Decimal('.01'))
+            volume = Decimal(str(list(json.loads(json.dumps(response.json()['Time Series (Daily)'])).values())[0]['5. volume'])).quantize(Decimal('.01'))
             quote = Quote(open, high, low, close, volume)
             print('%s,%s,%s,%s,%s' % (quote.open, quote.high, quote.low, quote.close, quote.volume))
             self.logger.write('Quote successfully retrieved (status code: [%s]) for ticker [%s]' % (response.status_code, ticker))
